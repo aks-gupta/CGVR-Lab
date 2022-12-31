7cea6a2dff8c3810c7010aaea880e780ba125c3a
Akshita Gupta <72127836+aks-gupta@users.noreply.github.com>
2022-12-31 20:14:15 +0530

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <GL/glut.h>
+#include <math.h>
+using namespace std;
+#define WINDOW_HEIGHT 500
+#define WINDOW_WIDTH 500
+int wid1, wid2, t;
+static GLfloat spin = 0.0;
+
+void display() {
+	glutSetWindow(t);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPushMatrix();
+	glLoadIdentity();
+	glRotatef(spin, 0, 0, 1);
+	glBegin(GL_POLYGON);
+	glColor3f(1, 0, 0);
+	glVertex3f(-0.25, -0.5, 0.25);
+	glColor3f(0, 1, 0);
+	glVertex3f(0.25, -0.5, 0.25);
+	glColor3f(0, 0, 1);
+	glVertex3f(0.25, 0.25, 0.25);
+	glColor3f(1, 0, 1);
+	glVertex3f(-0.25, 0.25, 0.25);
+	glEnd();
+	glPopMatrix();
+	if (t == wid1) {
+		glFlush(); //the matrix is emptied and the graphics are displayed
+		t = wid2;
+	}
+	else {
+		glutSwapBuffers(); //swaps front and back buffers
+		t = wid1;
+	}
+}
+
+void myreshape(int w, int h) {
+	glViewport(0, 0, w, h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	if (w <= h)
+		gluOrtho2D(-1.0, 1.0, -1.0 * (GLfloat)h / (GLfloat)w, 1.0 * (GLfloat)h / (GLfloat)w);
+	else
+		gluOrtho2D(-1.0 * (GLfloat)w / (GLfloat)h, 1.0 * (GLfloat)w / (GLfloat)h, -1.0, 1.0);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+}
+
+void myInit() {
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	glOrtho(-1, 1, -1, 1, -1, 1);
+	glClearColor(1, 1, 1, 1);
+	glColor3f(1, 0, 0);
+}
+
+void spinRect() {
+	spin += 0.05;
+	glutPostRedisplay(); //calls the display func
+}
+
+void mouse(int button, int state, int x, int y) {
+	switch (button) {
+	case GLUT_LEFT_BUTTON: if (state == GLUT_DOWN)
+		glutIdleFunc(spinRect);
+		break;
+	case GLUT_RIGHT_BUTTON: if (state == GLUT_DOWN)
+		glutIdleFunc(NULL);
+		break;
+	default: break;
+	}
+}
+
+int main(int argc, char* argv[]) {
+	glutInit(&argc, argv);
+	glutInitWindowPosition(100, 100);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	wid1 = glutCreateWindow("Single Buffer"); //displays using single buffer mode
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //results in jitter as the entire graphic may not be drawn
+	glutDisplayFunc(display);
+	glutReshapeFunc(myreshape);
+	myInit();
+
+	glutInitWindowPosition(600, 100);
+	glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+	wid2 = glutCreateWindow("Double Buffer"); //displays using double buffer mode 
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB); //no lag or jitter experienced
+	glutDisplayFunc(display); //2 buffers->(one for on-screen and another for calculating graphics)
+	glutReshapeFunc(myreshape);
+	glutMouseFunc(mouse);
+	myInit();
+
+	t = wid1;
+
+	glutMainLoop();
+	return 0;
+}
