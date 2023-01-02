cf3f71dc81cb1a3e2561e68b90256193be8d6601
Akshita Gupta <72127836+aks-gupta@users.noreply.github.com>
2023-01-02 19:10:49 +0530

    Update 3-rectangle-spin.cpp

--- 3-rectangle-spin.cpp
+++ 3-rectangle-spin.cpp
@@ -19,9 +19,9 @@ void display() {
 	glColor3f(0, 1, 0);
 	glVertex3f(0.25, -0.5, 0.25);
 	glColor3f(0, 0, 1);
-	glVertex3f(0.25, 0.25, 0.25);
+	glVertex3f(0.25, 0.5, 0.25);
 	glColor3f(1, 0, 1);
-	glVertex3f(-0.25, 0.25, 0.25);
+	glVertex3f(-0.25, 0.5, 0.25);
 	glEnd();
 	glPopMatrix();
 	if (t == wid1) {
